916e98ee296cd3b9a62829eeac9d2a645b89d5a8
Vonique Stricklen <vstrickl.git@gmail.com>
2023-11-24 13:02:31 -0800

    Created my first multiple choice quiz with CLIPS

--- quizzes/test_quiz.py
+++ quizzes/test_quiz.py
@@ -9,7 +9,6 @@ def run_quiz():
     module_dir = os.path.dirname(__file__)
     clips_file = os.path.join(module_dir, 'nfl_quiz.clp')
     environment.load(clips_file)
-    environment.reset()
 
     # Initialize the quiz
     environment.reset()
@@ -19,7 +18,12 @@ def run_quiz():
         if fact.template.name == "question":
             q_text = fact['text']
             q_id = fact['id']
-            print(f"Question: {q_text}")
+            option_a = fact['option-a']
+            option_b = fact['option-b']
+            option_c = fact['option-c']
+            option_d = fact['option-d']
+            print(f"Question {q_id}: {q_text}")
+            print(f" a) {option_a}\n b) {option_b}\n c) {option_c}\n d) {option_d}")
             user_answer = input("Your answer: ")
             response_fact = f"(response (question-id {q_id}) (answer \"{user_answer}\"))"
             environment.assert_string(response_fact)
@@ -28,16 +32,13 @@ def run_quiz():
     environment.run()
 
     # Retrieve the score
-    score_fact = None
     for fact in environment.facts():
         if fact.template.name == "score":
-            score_fact = fact
-            break
-
-    if score_fact:
-        correct = score_fact['correct']
-        incorrect = score_fact['incorrect']
-        print(f"Score: {correct} correct, {incorrect} incorrect")
+            correct = fact['correct']
+            incorrect = fact['incorrect']
+            total_questions = correct + incorrect
+            accuracy = round((correct / total_questions) * 100, 2) if total_questions > 0 else 0.0
+            print(f"\nQuiz Finished.\nTotal Correct: {correct}\nTotal Incorrect: {incorrect}\nYour Score: {accuracy}%")
 
 if __name__ == "__main__":
     run_quiz()
